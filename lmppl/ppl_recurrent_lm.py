fix recurrent LM perplexity normalization term

lmppl/ppl_recurrent_lm.py
@@ -102,9 +102,11 @@ class LM:
                 # compute loss
                 label = model_inputs['input_ids']
                 label[label == self.tokenizer.eos_token_id] = PAD_TOKEN_LABEL_ID
+                valid_length = (label != PAD_TOKEN_LABEL_ID).sum(dim=-1)
                 loss = self.loss_fct(output['logits'].view(-1, self.config.vocab_size), label.to(self.device).view(-1))
                 loss = loss.view(len(output['logits']), -1)
-                loss_list += torch.mean(loss, -1).cpu().tolist()
+                loss = torch.sum(loss, -1) / valid_length
+                loss_list += loss.tolist()
 
         # conversion to perplexity
         ppl = [exp(i) for i in loss_list]