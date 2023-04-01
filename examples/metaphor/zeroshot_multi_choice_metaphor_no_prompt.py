bd87c2c55bdbe07dc5ff5b593fbd2400031eb18c
asahi417 <asahi1992ushio@gmail.com>
2023-04-01 15:52:13 +0100

    add an exmaple

--- examples/metaphor/zeroshot_multi_choice_metaphor_no_prompt.py
+++ examples/metaphor/zeroshot_multi_choice_metaphor_no_prompt.py
@@ -104,8 +104,9 @@ def get_ppl(scoring_model, data, data_name, data_split, batch_size):
         ppls = scoring_model.get_perplexity(input_texts=[x[0] for x in dataset_flat], output_texts=[x[1] for x in dataset_flat], batch=batch_size)
         scores["perplexity"] = [{"input": x[0], "output": x[1], "score": float(p), "index": ind} for x, p, ind in zip(dataset_flat, ppls, dataset_index)]
     else:
-        print(dataset_flat[:2], len(dataset_flat))
+        print(dataset_flat[:2], len(dataset_flat), batch_size)
         ppls = scoring_model.get_perplexity(input_texts=dataset_flat[:2])
+        ppls = scoring_model.get_perplexity(input_texts=dataset_flat[:10])
         ppls = scoring_model.get_perplexity(input_texts=dataset_flat[:10], batch=batch_size)
         ppls = scoring_model.get_perplexity(input_texts=dataset_flat, batch=batch_size)
         scores["perplexity"] = [{"input": x, "output": "", "score": float(p), "index": ind} for x, p, ind in zip(dataset_flat, ppls, dataset_index)]
